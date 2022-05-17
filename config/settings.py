43a2eb42b6f79efb61ec8c32f558d8504f6d56f8
Aleksey Blagodetelev <darth-maul@mail.ru>
2022-05-17 18:26:31 +0500

    Preconfig

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = "django-insecure-#w+n-lkn$h%&i)+2@r5lm&(&ta+(avtqq+f4!=_v-x99%zw-xz"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
